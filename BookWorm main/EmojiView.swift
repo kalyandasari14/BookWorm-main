1b4216effa47f835c2400514995a6bc4297d858d
kalayn <kalyan>
2026-02-21 11:24:23 -0600

    main app finished and pushing into github

---
+++ BookWorm main/EmojiView.swift
@@ -0,0 +1,18 @@
+//
+//  EmojiView.swift
+//  BookWorm main
+//
+//  Created by kalyan on 2/20/26.
+//
+
+import SwiftUI
+
+struct EmojiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmojiView()
+}
---
+++ BookWorm main/RatingView.swift
@@ -0,0 +1,18 @@
+//
+//  Rating.swift
+//  BookWorm main
+//
+//  Created by kalyan on 2/19/26.
+//
+
+import SwiftUI
+
+struct Rating: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Rating()
+}
